0ce339068d38a5af2bd5f0234626b3cdde3ea179
TheClashFruit <clash.fruit123@gmail.com>
2021-07-01 20:06:05 +0200

    🆕 Add more pages to be supported

--- websites/Z/ZryteZene/presence.ts
+++ websites/Z/ZryteZene/presence.ts
@@ -13,6 +13,7 @@ let date: Date;
 let startTime: number;
 let endTime: number;
 let musicLen: string;
+let asset = "error";
 
 date = new Date();
 startTime = date.getTime();
@@ -20,6 +21,15 @@ startTime = date.getTime();
 const slider = document.getElementsByClassName('slider-play')[0] as HTMLInputElement;
 
 function myOutsideHeavyLiftingFunction(){
+  if(document.location.href.includes('profile')) {
+    music = "Viewing " + document.getElementsByClassName('text-xl')[0].innerHTML + "'s Profile Page";
+    asset = "profile";
+    uploader = "";
+  } else if(document.location.href.includes('upload')) {
+    music = "Uploading a Music";
+    asset = "upload";
+    uploader = "";
+  } else if(document.location.href == 'https://zrytezene.thatcakeid.com/') {
     uploader = document.getElementsByClassName('text-xs')[0].innerHTML;
     music = document.getElementsByClassName('title-custom')[0].innerHTML;
   
@@ -27,20 +37,27 @@ function myOutsideHeavyLiftingFunction(){
 
     if(music == '' && uploader == '') {
       music = "Viewing Home Page";
+      asset = "home";
       uploader = "";
     } else {
       uploader = "Uploaded By " + uploader;
+      asset = "music";
       date = new Date();
       startTime = date.getTime();
       endTime = date.getTime() + ((parseInt(musicLen) - parseInt(slider.value)) * 1000);
     }
+  } else {
+    music = "Page Not Defined In Presence";
+    asset = "error";
+    uploader = "";
+  }
 }
 
 setInterval(myOutsideHeavyLiftingFunction, 1000);
 
 presence.on("UpdateData", async () => {
   const presenceData: PresenceData = {
-    largeImageKey: "music",
+    largeImageKey: asset,
     smallImageKey: "icon",
     smallImageText: "ZryteZene",
     details: music,
